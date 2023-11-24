f13f0cba8acfcb1dbe55665a91fc1d0e72729943
MarcoChanwoo <cwhong8087@gmail.com>
2023-11-25 01:03:31 +0900

    updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import TodoInsert from './components/TodoInsert';
 import TodoTemplate from './components/TodoTemplate';
 import TodoList from './components/TodoList';
+import { useState } from 'react';
 
 const App = () => {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: '리액트 기초 공부',
+      checked: true,
+    },
+    {
+      id: 2,
+      text: '컴포넌트 스타일링',
+      checked: true,
+    },
+    {
+      id: 3,
+      text: '다양한 어플 제작',
+      checked: false,
+    },
+  ]);
+
   return (
     <TodoTemplate>
       <TodoInsert />
-      <TodoList />
+      <TodoList todos={todos} />
     </TodoTemplate>
   );
 };
