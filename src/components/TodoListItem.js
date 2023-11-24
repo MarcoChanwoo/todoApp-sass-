d05f86fe62e1d6663b68734c859965d154271ae2
MarcoChanwoo <cwhong8087@gmail.com>
2023-11-25 00:40:13 +0900

    updated

--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -4,3 +4,11 @@ import {
   MdRemoveCircleOutline,
 } from 'react-icons/md';
 import './TodoListItem.scss';
+
+const TodoListItem = () => {
+  return (
+    <div className="TodoListItem">
+      <div className="checkbox"></div>
+    </div>
+  );
+};
