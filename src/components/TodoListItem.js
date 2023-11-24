f2ad1ca96e1fdbb4c1ed973c1e171099be0737d5
MarcoChanwoo <cwhong8087@gmail.com>
2023-11-25 00:42:24 +0900

    updated

--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -8,7 +8,15 @@ import './TodoListItem.scss';
 const TodoListItem = () => {
   return (
     <div className="TodoListItem">
-      <div className="checkbox"></div>
+      <div className="checkbox">
+        <MdCheckBoxOutlineBlank />
+        <div className="text">할 일</div>
+      </div>
+      <div className="remove">
+        <MdRemoveCircleOutline />
+      </div>
     </div>
   );
 };
+
+export default TodoListItem;
